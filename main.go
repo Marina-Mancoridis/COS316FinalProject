418896c829169be4ad474891fec4642698daf8a1
Marina-Mancoridis <mm96@princeton.edu>
2022-12-10 18:47:59 -0500

    implemented priority with queue

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ package main
 // algorithms, outputting statistics of how the CPU runs under each
 // algorithm
 func main() {
-	processes := generatePriorityAgingProcesses(5)
+	processes := generatePriorityAgingProcesses(100)
 	// processes := generateShortLongProcesses(100, 900)
 	// FirstComeFirstServe(processes, 500)
 	// ShortestJobFirst(processes, 500)
 	// Priority(processes, 500)
 	PriorityWithAging(processes, 100)
+	// PriorityWithQueue(processes, 100)
 	// RoundRobin(processes, 5, 1)
 }
--- schedulers.go
+++ schedulers.go
@@ -77,7 +77,7 @@ func FirstComeFirstServe(processes []Process, totalTime int) {
 	}
 	// fmt.Println("\n")
 
-	// outputs statistics for first come first serve scheduling algorithm
+	// outputs statistics
 	GenerateStatistics(currentTime, processes)
 }
 
@@ -115,7 +115,7 @@ func ShortestJobFirst(processes []Process, totalTime int) {
 	}
 	// fmt.Println("\n")
 
-	// outputs statistics for shortest job first scheduling algorithm
+	// outputs statistics
 	GenerateStatistics(currentTime, processes)
 }
 
@@ -154,7 +154,7 @@ func Priority(processes []Process, totalTime int) {
 	}
 	// fmt.Println("\n")
 
-	// outputs statistics for shortest job first scheduling algorithm
+	// outputs statistics
 	GenerateStatistics(currentTime, processes)
 }
 
@@ -212,7 +212,7 @@ func Priority(processes []Process, totalTime int) {
 // 	}
 // 	fmt.Println("\n")
 
-// 	// outputs statistics for shortest job first scheduling algorithm
+// 	// outputs statistics
 // 	GenerateStatistics(currentTime, processes)
 // }
 
@@ -305,6 +305,92 @@ func PriorityWithAging(processes []Process, totalTime int) {
 	}
 	fmt.Println("\n")
 
-	// outputs statistics for shortest job first scheduling algorithm
+	// outputs statistics
+	GenerateStatistics(currentTime, processes)
+}
+
+// priority algorithm with queue and without aging
+func PriorityWithQueue(processes []Process, totalTime int) {
+	fmt.Println("\n\n                         Running Priority (With Queue) Scheduling Algorithm...")
+
+	// sorts the list of processes by arrival time
+	sort.Slice(processes, func(i, j int) bool {
+		return processes[i].arrivalTime < processes[j].arrivalTime
+	})
+
+	// fmt.Println("PROCESSES SORTED BY ARRIVAL TIME")
+	// printProcesses(processes)
+	// fmt.Println("--------------")
+
+	i := 0
+	currentTime := 0
+
+	for currentTime < totalTime {
+		// fmt.Println("---------------------------------------------------")
+		// fmt.Println("AT TIME STEP ", currentTime)
+		// add to queue
+		for j := 0; j < len(processes); j++ {
+			if processes[j].arrivalTime > currentTime {
+				break
+			}
+			// this is the only part that I changed ... -marm
+			if !processes[j].completed {
+				// process was not in queue -> add it
+				if !processes[j].isInQueue {
+					processes[j].isInQueue = true
+				}
+				// no need to update any processes who haven't arrived yet
+			}
+		}
+
+		// fmt.Println("queue and priorities supposedly updated...")
+		// printProcesses(processes)
+		// fmt.Println("--------------")
+
+		// find the next process to execute
+		processId := -1
+		lowestPriority := math.MaxInt
+		for j := 0; j < len(processes); j++ {
+			if processes[j].isInQueue {
+				// note: between processes of same priority, processes that arrived first are prioritized
+				if processes[j].priority < lowestPriority {
+					processId = j
+					lowestPriority = processes[j].priority
+				}
+			}
+		}
+		// fmt.Println("next process to execute has id: ", processId)
+
+		// return if there are no more processes to execute
+		if processId == -1 {
+			return
+		}
+
+		// execute the next process, mark as completed, take it off the queue
+		if currentTime+processes[processId].duration <= totalTime {
+			currentTime += processes[processId].duration
+			processes[processId].completed = true
+			processes[processId].isInQueue = false
+			processes[processId].turnaroundTime += processes[processId].duration
+		} else {
+			break
+		}
+
+		// update the waiting and turnaround time of all processes in the queue
+		for j := 0; j < len(processes); j++ {
+			if processes[j].isInQueue {
+				processes[j].waitingTime += processes[processId].duration
+				processes[j].turnaroundTime += processes[processId].duration
+			}
+		}
+
+		if i >= len(processes)-1 {
+			break
+		}
+		i++
+	}
+	fmt.Println("\n")
+
+	// outputs statistics
 	GenerateStatistics(currentTime, processes)
 }
