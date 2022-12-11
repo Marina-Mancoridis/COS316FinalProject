c8c42fc3b98bbdc4674f5a2c16b402fe2e9330db
Marina-Mancoridis <mm96@princeton.edu>
2022-12-10 19:15:26 -0500

    implemented fcfs with queue

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ package main
 // algorithms, outputting statistics of how the CPU runs under each
 // algorithm
 func main() {
-	processes := generatePriorityAgingProcesses(100)
+	processes := generatePriorityAgingProcesses(5)
 	// processes := generateShortLongProcesses(100, 900)
 	// FirstComeFirstServe(processes, 500)
 	// ShortestJobFirst(processes, 500)
 	// Priority(processes, 500)
-	PriorityWithAging(processes, 100)
+	// PriorityWithAging(processes, 100)
+	FirstComeFirstServeQueue(processes, 100)
 	// PriorityWithQueue(processes, 100)
 	// RoundRobin(processes, 5, 1)
 }
