17dac2ec6819fdfd7c59ad77cfd962a47dbda285
Marina-Mancoridis <mm96@princeton.edu>
2022-12-11 16:33:47 -0500

    boiler plate files for mlqs algs

---
+++ mlqsRR.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// runs the list of processes for a maximum of totalTime seconds in
+// accordance to the round robin multi level queue scheduling algorithm
+func RoundRobinMultiLevelQueueScheduling(processes []Process, totalTime int) {
+	fmt.Println("\n\n                         Running Round Robin Multi Level Queue Scheduling Algorithm...")
+}
---
+++ mlqsStrict.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// runs the list of processes for a maximum of totalTime seconds in
+// accordance to the strict multi level queue scheduling algorithm
+func StrictMultiLevelQueueScheduling(processes []Process, totalTime int) {
+	fmt.Println("\n\n                         Running Strict Multi Level Queue Scheduling Algorithm...")
+}
